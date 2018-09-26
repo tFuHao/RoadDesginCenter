c30a2f2e95b3aa557d7ad5335e421d5668f6869d
freedomforme <spzhanggeng@outlook.com>
2018-09-26 20:27:00 +0800

    完成断链要素表

---
+++ SSKJ.RoadDesignCenter.Utility/Tools/FileUtils.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace SSKJ.RoadDesignCenter.Utility.Tools
+{
+    public static class FileUtils
+    {
+        public static string SaveFile(string rootPath, IFormFile file)
+        {
+            var webRootPath = rootPath;
+            var path = webRootPath + "/upload/import";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            var fullPath = path + '/' + file.FileName;
+            fullPath = fullPath.Replace("\\", "/");
+            using (FileStream fs = new FileStream(fullPath, FileMode.Create))
+            {
+                file.CopyTo(fs);
+                fs.Flush();
+                fs.Close();
+            }
+            return fullPath;
+        }
+
+        public static void DeleteFile(string path)
+        {
+            if (System.IO.File.Exists(path))
+            {
+                try
+                {
+                    System.IO.File.Delete(path);
+                }
+                catch (IOException)
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
