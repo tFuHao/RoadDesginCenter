14b9ad732169961594723d1e2200a5685835025a
freedomforme <spzhanggeng@outlook.com>
2018-09-27 11:38:41 +0800

    111

---
+++ SSKJ.RoadDesignCenter.IRepository/Project/RouteElement/IVerticalCurve_GradeChangePointRepository.cs
@@ -0,0 +1,9 @@
+﻿using SSKJ.RoadDesignCenter.Models.ProjectModel;
+
+namespace SSKJ.RoadDesignCenter.IRepository.Project.RouteElement
+{
+    public interface IVerticalCurve_GradeChangePointRepository : IBaseRepository<VerticalCurve_GradeChangePoint>
+    {
+        
+    }
+}
