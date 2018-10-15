46e7f07c509e91c3267750754448e376ef4f5978
741051180@qq.com <741051180@qq.com>
2018-10-15 14:04:58 +0800

    Login

--- SSKJ.RoadDesignCenter.API/Controllers/LoginController.cs
+++ SSKJ.RoadDesignCenter.API/Controllers/LoginController.cs
@@ -25,12 +25,14 @@ namespace SSKJ.RoadDesignCenter.API.Controllers
     public class LoginController : Controller
     {
         private readonly IUserProjectBusines userProjectBll;
+        private readonly IUserBusines sysUserBll;
         private readonly IBusines.Project.IUserBusines prjUserBll;
 
-        public LoginController(IBusines.Project.IUserBusines prjUserBll, IUserProjectBusines userProjectBll)
+        public LoginController(IBusines.Project.IUserBusines prjUserBll, IUserProjectBusines userProjectBll, IUserBusines sysUserBll)
         {
             this.prjUserBll = prjUserBll;
             this.userProjectBll = userProjectBll;
+            this.sysUserBll = sysUserBll;
         }
         //public IActionResult Index()
         //{
@@ -42,36 +44,60 @@ namespace SSKJ.RoadDesignCenter.API.Controllers
         {
             try
             {
+                var _user = new UserInfoModel();
                 if (string.IsNullOrEmpty(model.ProjectCode))
-                    return BadRequest(new { message = "项目代码不能为空，请输入后再试!" });
+                {
+                    if (model.UserName.ToLower() == "system")
+                    {
+                        if (model.Password == "123456")
+                            _user.UserId = _user.RoleId = "System";
+                        else
+                            return BadRequest(new { type = 0, message = "密码错误，请重新输入!" });
+                    }
+                    else
+                    {
+                        var user = await sysUserBll.GetEntityAsync(u => u.Account == model.UserName);
+                        if (user == null)
+                            return BadRequest(new { type = 0, message = "用户名错误或用户名不存在，请重新输入!" });
+                        else
+                        {
+                            if (user.Password != model.Password)
+                                return BadRequest(new { type = 0, message = "密码错误，请重新输入!" });
+                        }
+                        _user = Utility.Tools.MapperUtils.MapTo<RoadDesignCenter.Models.SystemModel.User, UserInfoModel>(user);
+                        _user.RoleId = "PrjManager";
+                    }
+                }
+                else
+                {
+                    var entity = await userProjectBll.GetEntityAsync(p => p.PrjIdentification == model.ProjectCode);
 
-                var entity = await userProjectBll.GetEntityAsync(p => p.PrjIdentification == model.ProjectCode);
+                    if (entity == null)
+                        return BadRequest(new { type = 0, message = "项目代码有误或不存在，请重新输入!" });
 
-                if (entity == null)
-                    return BadRequest(new { message = "项目代码有误或不存在，请重新输入!" });
+                    if (string.IsNullOrEmpty(entity.PrjDataBase))
+                        return BadRequest(new { type = 0, message = "出错了，请稍后重试!" });
 
-                if (string.IsNullOrEmpty(entity.PrjDataBase))
-                    return BadRequest(new { message = "出错了，请稍后重试!" });
+                    var user = await prjUserBll.GetEntityAsync(u => u.Account == model.UserName && u.Password == model.Password, entity.PrjDataBase);
 
-                var user = await prjUserBll.GetEntityAsync(u => u.Account == model.UserName && u.Password == model.Password, entity.PrjDataBase);
+                    if (user == null)
+                        return BadRequest(new { type = 0, message = "用户名或密码错误，请重新输入!" });
 
-                if (user.EnabledMark == 0)
-                    return BadRequest(new { message = "该角色已被锁定，请联系管理员解锁" });
+                    if (user.EnabledMark == 0)
+                        return BadRequest(new { type = 0, message = "该角色已被锁定，请联系管理员解锁" });
 
-                if (user == null)
-                    return BadRequest(new { message = "用户名或密码错误，请重新输入!" });
-
-                var _user = Utility.Tools.MapperUtils.MapTo<User, UserInfoModel>(user);
-                _user.DataBaseName = entity.PrjDataBase;
+                    _user = Utility.Tools.MapperUtils.MapTo<User, UserInfoModel>(user);
+                    _user.DataBaseName = entity.PrjDataBase;
+                }
                 _user.TokenExpiration = DateTime.Now.AddDays(1);
 
                 string token = Utility.Tools.TokenUtils.ToToken(_user);
 
-                return Ok(new { code = 1, token });
+                return Ok(new { type = 1, role = _user.RoleId, code = 1, token });
             }
             catch (Exception)
             {
-                return BadRequest(new { message = "出错了，请稍后重试!" });
+                return BadRequest(new { type = 0, message = "出错了，请稍后重试!" });
             }
         }
 
