2f86ceaa6751f34fdf90a9bc54340e1a3c3bbe56
freedomforme <spzhanggeng@gmail.com>
2018-11-03 10:41:31 +0800

    1

---
+++ SSKJ.RoadDesignCenter.API/Areas/RouteData/Models/VerticalCurveInputDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSKJ.RoadDesignCenter.API.Areas.RouteData.Models
+{
+    public class VerticalCurveInputDto
+    {
+        public string VerticalCurveId { get; set; }
+        public string RouteId { get; set; }
+        public int? VerticalCurveType { get; set; }
+        public int? GradeChangePointNumber { get; set; }
+        public int? CurveNumber { get; set; }
+        public double? VerticalCurveLength { get; set; }
+        public double? BeginStake { get; set; }
+        public double? EndStake { get; set; }
+        public string Description { get; set; }
+    }
+}
